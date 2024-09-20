6addd9e7780fea98ec36858c7d3c45057db79a02
Ezequiel Santalla <ezequielasantalla@gmail.com>
2024-09-20 20:09:14 -0300

    adding comments to the exam

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         ProductService productService = new ProductService();
 
+        // Exercise 3)
         Product chair = new Chair(10, "Gaming Chair", 15000.0, KindOfProduct.FORNITURE, true);
         Product desk = new Desk(12, "Oak Desk", 18000.0, KindOfProduct.FORNITURE, 1.05, 2.3);
         Product notebook = new Notebook(10, "Notebook Lenovo", 26000.0, KindOfProduct.TECH_PRODUCT, "Iturria", 256);
@@ -21,6 +22,18 @@ public class Main {
         productService.showProductList();
         System.out.println();
 
+        // Exercise 4)
+        System.out.println("Chair price before the discount: " + chair.getPrice());
+        chair.setPrice(productService.applyDiscount(chair, 0.5));
+        System.out.println("Chair price after the discount: " + chair.getPrice());
+        System.out.println();
+
+        System.out.println("Printer price before the discount: " + printer.getPrice());
+        printer.setPrice(productService.applyDiscount(printer, 0.3));
+        System.out.println("Printer price after the discount: " + printer.getPrice());
+        System.out.println();
+
+        // Exercise 5)
         System.out.println("Product list with normal prices");
         System.out.println();
         productService.showList();
@@ -30,15 +43,5 @@ public class Main {
         System.out.println();
         productService.updatePrices();
         productService.showList();
-
-        System.out.println();
-        System.out.println("Chair price before the discount: " + chair.getPrice());
-        chair.setPrice(productService.applyDiscount(chair, 0.5));
-        System.out.println("Chair price after the discount: " + chair.getPrice());
-        System.out.println();
-
-        System.out.println("Printer price before the discount: " + printer.getPrice());
-        printer.setPrice(productService.applyDiscount(printer, 0.3));
-        System.out.println("Printer price after the discount: " + printer.getPrice());
     }
 }
