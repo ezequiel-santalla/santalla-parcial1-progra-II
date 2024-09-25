f49a821760bcee94aa64d2f57a77838a3e82eb88
Ezequiel Santalla <ezequielasantalla@gmail.com>
2024-09-25 13:33:02 -0300

    parcial 1 corregido

--- src/model/Furniture.java
+++ src/model/Furniture.java
@@ -3,16 +3,12 @@ package model;
 import enums.KindOfProduct;
 
 public class Furniture extends Product {
-    private String nameManufacturer;
-
     public Furniture(Integer stock, String name, Double price, KindOfProduct kindOfProduct) {
         super(stock, name, price, kindOfProduct);
     }
 
     @Override
     public String toString() {
-        return "TechProduct{" +
-                "nameManufacturer='" + nameManufacturer + '\'' +
-                "} " + super.toString();
+        return "Furniture{} " + super.toString();
     }
 }
